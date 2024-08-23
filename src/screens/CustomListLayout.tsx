ade48a298582dd211f77ccefa9f4d8a9947f0a33
danilojr7 <danilo.2023014102@alunocesul.com>
2024-08-23 19:24:21 -0300

    first commit

---
+++ src/screens/CustomListLayout.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+interface CustomListLayoutProps {
+  name: string;
+  color: string;
+  numeros: number;
+}
+
+const CustomListLayout: React.FC<CustomListLayoutProps> = ({ name, color, numeros }) => {
+  return (
+    <View style={[styles.listContainer, { backgroundColor: color }]}>
+      <Text style={styles.listText}>{name}</Text>
+      <Text style={styles.listNumber}>{numeros}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  listContainer: {
+    padding: 16,
+    marginBottom: 10,
+    borderRadius: 50,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  listText: {
+    fontSize: 18,
+    color: '#fff',
+  },
+  listNumber: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
+
+export default CustomListLayout;
