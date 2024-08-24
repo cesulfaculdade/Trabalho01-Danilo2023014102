9e1eb29e6d1ecf5dad95f0974470d49adc39b591
Danilo Marcon <danilo.2023014102@alunocesul.com>
2024-08-24 03:18:25 -0300

    Criação do botão e da FlatList

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,45 +1,81 @@
-import React from "react";
-import { StyleSheet, Text, TextInput, View, Image, TouchableOpacity } from "react-native";
-import CustomListLayout from './CustomListLayout';
-import shopping from '../../src/assets/shopping_list.png'
-import { MaterialIcons } from '@expo/vector-icons';
+import { Alert, FlatList, Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
+import { MaterialIcons } from '@expo/vector-icons';
+import shopping from '../../src/assets/shopping_list.png'
+import { useState } from "react";
+import { Product } from "../components/Product";
 
-export default function Home() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.secaoRoxa}>
+export function Home() {
+
+  const [products, setProducts] = useState<string[]>([]);
+  const [productName, setProductName] = useState('');
+
+  function handleProductAdd() {
+      if (products.includes(productName)) {
+          return Alert.alert("Produto já cadastrado!", "Já existe um produto na lista com este nome.");
+      }
+
+      setProducts((prevState) => [...prevState, productName]);
+      setProductName('');
+    }
+
+  function handleProductRemove(name: string) {
+    Alert.alert("Remover", `Deseja remover o produto ${name}?`, [
+            {
+              text: 'Sim',
+              onPress: () => setProducts(prevState => prevState.filter(product => product != name)) 
+            },
+            {
+              text: 'Não',
+              style: 'cancel'
+            }
+        ]);
+    }
+
+    return (
+  <View style={styles.container}>
+
+      <View style={styles.purpleContainer}>
         <Text style={styles.title}>Lista de Compras</Text>
       </View>
 
-      <View style={styles.form}>
-        <TextInput
-          style={styles.input}
-          placeholder="Adicione um novo produto"
-          placeholderTextColor="#BDBABA"
-          keyboardType="default"
-        />
+        <View style={styles.form}>
+          <View style={styles.inputContainer}>
+            <TextInput style={styles.input}
+              placeholder="Adicione um novo produto"
+              placeholderTextColor='#808080'
+              keyboardType='default'
+              onChangeText={setProductName} 
+              value={productName}
+                />
 
-          <TouchableOpacity style={styles.button}>
-              <MaterialIcons style={styles.buttontxt} name="add-circle-outline" size={16} color="white" />
-            </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleProductAdd}>
+            <MaterialIcons name="add-circle-outline" size={16} color="white" />
+              </TouchableOpacity>
+        </View>
             
-        </View>
-    
-      <View style={styles.secaoBranca}>
-        <View style={styles.containerText}>
-          <CustomListLayout
-            name={"Produtos"}
-            numeros={0}
+        <FlatList
+              data={products}
+              keyExtractor={item => item}
+                renderItem={({ item }) => (
+                <Product nome={item} onRemove={() => handleProductRemove(item)}>
+                </Product>
+            )}
+              showsVerticalScrollIndicator={false}
+              contentContainerStyle={products.length <= 0 && styles.list}
+              ListEmptyComponent={() => (
+                <View style={styles.emptyListContainer} >
+                    <Image source={shopping}></Image> 
+                        <Text style={styles.secoundText}>
+                            Você ainda não tem produtos na lista de compra
+                        </Text>
+                          <Text style={styles.textSubtitle}>
+                            Adicione produtos e organize sua lista de compras
+                        </Text>
+                  </View>
+              )}   
           />
-          <CustomListLayout
-            name={"Finalizados"}
-            numeros={0}
-          />
-        </View>
-
-        <Image source={shopping}/>
       </View>
-    </View>
-  );
+  </View>
+  )
 }
