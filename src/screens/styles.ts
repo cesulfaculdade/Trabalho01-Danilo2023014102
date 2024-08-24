3cbc1478bf2d80731a71258842dddd96b526b5d9
Danilo Marcon <danilo.2023014102@alunocesul.com>
2024-08-24 04:26:20 -0300

    reformulação screen style

--- src/screens/styles.ts
+++ src/screens/styles.ts
@@ -2,59 +2,71 @@ import { StyleSheet } from "react-native";
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export const styles = StyleSheet.create({
-        container: {
-          flex: 1,
-          flexDirection: "column"
-        },
-        secaoRoxa: {
-          flex: 2,
-          backgroundColor: "#7A4A9E",
-          justifyContent: 'center',
-          alignItems: 'center'
-        },
-        input: {
-          flex: 1,
-          backgroundColor: "#fff",
-          height: 54,
-          padding: 16,
-          fontSize: 18,
-          borderRadius: 5,
-          marginRight: 16,
-          borderWidth: 0.5,
-        },
-        form: {
-          justifyContent: 'center',
-          flexDirection: "row",
-          height: 54,
-          paddingLeft: 24,
-          paddingRight: 24,
-          top: -30
-        },
-        secaoBranca: {
-          flex: 7,
-          backgroundColor: "#FFF"
-        },
-        containerText: {
-          padding: 20,
-        },
-        title: {
-          color: "#FFF",
-          fontSize: 24,
-          fontWeight: "bold"
-        },
-        button: {
-          borderRadius: 5,
-          backgroundColor: '#31C667',
-          height: 54,
-          width: 52,
-          marginLeft: -3,
-          justifyContent: "center",
-          alignItems: "center",
-          borderWidth: 0.5
-          
-          },
-        buttontxt:{
-          margin: 'auto'
-        }  
-    
-    })
+    container: {
+        flex: 1,
+        backgroundColor: '#f2f2f2',
+        width: '100%'
+    },
+    purpleContainer: {
+        height: 173,
+        backgroundColor: '#7A4A9E'
+    },
+    title: {
+        color: '#F2F2F2',
+        fontSize: 22,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        alignItems: 'center',
+        margin: 'auto'
+    },
+    button: {
+        width: 54,
+        height: 54,
+        backgroundColor: '#31C667',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 5,
+        marginLeft: 4,
+        borderWidth: 0.5
+    },
+    input: {
+        backgroundColor: 'white',
+        fontSize: 16,
+        flex: 1,
+        height: 54,
+        borderRadius: 5,
+        paddingHorizontal: 16,
+        borderWidth: 0.5,
+        borderColor: '#808080'
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        top: -30,
+        alignItems: 'center'
+    },
+    form: {
+        paddingHorizontal: 24
+    },
+    secoundText: {
+        color: '#808080',
+        textAlign: 'center',
+        fontSize: 14,
+        fontWeight: 'bold'
+    },
+    textSubtitle: {
+        color: '#808080',
+        fontSize: 14,
+        textAlign: "center",
+    },
+    emptyListContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    image: {
+        width: 56,
+        right: 56
+    },
+    list: {
+        justifyContent: 'center'
+    }
+})
